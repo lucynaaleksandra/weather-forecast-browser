c1ed76571a028d23463f791bcc7988699b2bacd8
Lucyna Aleksandra <lucy@lucynaaleksandra.com>
2018-07-21 17:56:05 -0400

    work in progress

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,13 +1,23 @@
 import React from 'react'
-import { Sparklines, SparklinesLine } from 'react-sparklines'
+import _ from 'lodash'
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines'
 
+
+function average() {
+  return _.round(_.sum(data)/data.length)
+}
 
 export default (props) => {
   return (
     <div>
       <Sparklines height={120} width={180} data={props.data}>
-        <SparklinesLine color={props.color}></SparklinesLine> 
+        <SparklinesLine color={props.color} />
+        <SparklinesReferenceLine type='avg' />
       </Sparklines>
+      <div>
+        {/* calculate avg, pass array of data to that func */}
+        {average(props.data)} {props.units}
+      </div>
     </div>
   )
 }
--- src/reducers/weatherReducer.js
+++ src/reducers/weatherReducer.js
@@ -1,7 +1,7 @@
 import { FETCH_WEATHER } from '../actions/fetchWeather'
 
 export default function weatherReducer(state = [], action) {
-  console.log("action received action")
+  // console.log("action received action")
   switch(action.type) {
     case FETCH_WEATHER: 
       return [
